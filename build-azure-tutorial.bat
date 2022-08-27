c1301e0d3baaee958c33ffe17bbf6673f6024b9d
Saman Alishirishahrbabak <samanalishiri@gmail.com>
2022-08-27 22:07:48 +0200

    temp

--- build-azure-tutorial.bat
+++ build-azure-tutorial.bat
@@ -1,4 +1,5 @@
 call mvn -f .\azure-tutorial\pom.xml clean package -DskipTests=true
 call mvn -f .\azure-tutorial\pom.xml test -Dvendor=Azurite
+call mvn -f .\azure-tutorial\pom.xml test -Dvendor=AzureEnv
 
 call del .\azure-tutorial\storage-tutorial\src\test\resources\temp-test-result_*
